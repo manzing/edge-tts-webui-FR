cc15ea73c9f38e90d703f93986dc8e7b7cef137a
manzing <manzing@gmail.com>
2024-01-20 16:25:22 +0000

    Update app.py
    - Ajout des modèles de voix FR de Edge-TTS
    - Traduction de l'interface
    - Modification des exemples

--- app.py
+++ app.py
@@ -4,23 +4,20 @@ import asyncio
 import os
 # https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=6A5AA1D4EAFF4E9FB37E23D68491D6F4
 SUPPORTED_VOICES = {
-    'Xiaoxiao-晓晓': 'zh-CN-XiaoxiaoNeural',
-    'Xiaoyi-晓伊': 'zh-CN-XiaoyiNeural',
-    'Yunjian-云健': 'zh-CN-YunjianNeural',
-    'Yunxi-云希': 'zh-CN-YunxiNeural',
-    'Yunxia-云夏': 'zh-CN-YunxiaNeural',
-    'Yunyang-云扬': 'zh-CN-YunyangNeural',
-    'liaoning-Xiaobei-晓北辽宁': 'zh-CN-liaoning-XiaobeiNeural',
-    'shaanxi-Xiaoni-陕西晓妮': 'zh-CN-shaanxi-XiaoniNeural'
+    'Vivienne': 'fr-FR-VivienneMultilingualNeural',
+    'Denise': 'fr-FR-DeniseNeural',
+    'Eloise': 'fr-FR-EloiseNeural',
+    'Remy': 'fr-FR-RemyMultilingualNeural',
+    'Henri': 'fr-FR-HenriNeural'
 }
 
-# 发音切换
+# Modification de voix
 def changeVoice(voices):
     example = SUPPORTED_VOICES[voices]
-    example_file = os.path.join(os.path.dirname(__file__), "example/"+example+".wav")
+    example_file = os.path.join(os.path.dirname(__file__), "example/"+example+".mp3")
     return example_file
 
-# 文本转语音
+# Text to Speech
 async def textToSpeech(text, voices, rate, volume):
     output_file = "output.mp3"
     voices = SUPPORTED_VOICES[voices]
@@ -42,11 +39,11 @@ async def textToSpeech(text, voices, rate, volume):
     if (os.path.exists(audio_file)):
         return audio_file
     else:
-        raise gr.Error("转换失败！")
+        raise gr.Error("La création a échoué！")
         return FileNotFoundError
 
 
-# 清除转换结果
+# Effacer le texte et rendu
 def clearSpeech():
     output_file = os.path.join(os.path.dirname(__file__), "output.mp3")
     if (os.path.exists(output_file)):
@@ -54,28 +51,27 @@ def clearSpeech():
     return None, None
 
 
-with gr.Blocks(css="style.css", title="文本转语音") as demo:
+with gr.Blocks(css="style.css", title="Text To Speech") as demo:
     gr.Markdown("""
-    # 微软Edge文本转语音
-    调用edge-tts 进行转换
+    # Text-to-Speech FR
+    Conversion texte en audio vocal avec edge-tts
     """)
     with gr.Row():
         with gr.Column():
-            text = gr.TextArea(label="文本", elem_classes="text-area")
-            btn = gr.Button("生成", elem_id="submit-btn")
+            text = gr.TextArea(label="Entrez votre texte", elem_classes="text-area")
+            btn = gr.Button("Convertir en audio vocal", elem_id="submit-btn")
         with gr.Column():
             voices = gr.Dropdown(choices=[
-                "Xiaoxiao-晓晓", "Xiaoyi-晓伊", "Yunjian-云健", "Yunxi-云希",
-                "Yunxia-云夏", "Yunyang-云扬", "liaoning-Xiaobei-晓北辽宁",
-                "shaanxi-Xiaoni-陕西晓妮"
+                "Vivienne", "Denise", "Eloise", "Remy",
+                "Henri"
             ],
-                                 value="Xiaoxiao-晓晓",
-                                 label="发音",
-                                 info="请选择发音人",
+                                 value="Vivienne",
+                                 label="Modèle de voix",
+                                 info="Choix du modèle vocal FR (France)",
                                  interactive=True)
             
-            example = gr.Audio(label="试听",
-                              value="example/zh-CN-XiaoxiaoNeural.wav",
+            example = gr.Audio(label="Exemple",
+                              value="example/fr-FR-VivienneMultilingualNeural.mp3",
                               interactive=False,
                               elem_classes="example")
 
@@ -84,21 +80,21 @@ with gr.Blocks(css="style.css", title="文本转语音") as demo:
                              100,
                              step=1,
                              value=0,
-                             label="语速增减",
-                             info="加快或减慢语速",
+                             label="Vitesse de parole",
+                             info="Accélérer ou ralentir la voix",
                              interactive=True)
             
             volume = gr.Slider(-100,
                                100,
                                step=1,
                                value=0,
-                               label="音调增减",
-                               info="加大或减小音调",
+                               label="Volume",
+                               info="Augmenter ou baisser le volume audio",
                                interactive=True)
-            audio = gr.Audio(label="输出",
+            audio = gr.Audio(label="Sortie",
                              interactive=False,
                              elem_classes="audio")
-            clear = gr.Button("清除", elem_id="clear-btn")
+            clear = gr.Button("Effacer", elem_id="clear-btn")
             btn.click(fn=textToSpeech,
                       inputs=[text, voices, rate, volume],
                       outputs=[audio])
